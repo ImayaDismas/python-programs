1e0fb9af95743c76bb4952a9824a09dc6b8ca77f
ImayaDismas <imayadismas@gmail.com>
2016-02-02 16:42:08 +0300

    syntay with whitespaces

---
+++ syntaxwhitespaces.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#/usr/bin/python3 path to the python3 interpreter
+def main():#control structure
+	print("This is the syntax.py file.")
+	egg()
+print("this is another line")
+	
+
+def egg():
+	print("egg")
+
+if __name__ == "__main__": main()
+#indentation should be consistent all through the program
