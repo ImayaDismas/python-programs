fa53f96f4312da889f74df52c1a2b90f7f29070e
ImayaDismas <imayadismas@gmail.com>
2016-02-03 23:07:29 +0300

    Objects in python

---
+++ program.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Objects are the functions variables and code
+# every object has an id , type and value
+def main():
+	x = 42
+	print(x)
+
+	print(id(x))
+
+	print(type(x))
+
+if __name__ == "__main__": main()
