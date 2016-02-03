99101662ccd698a9482e461c9bba0096b993663b
ImayaDismas <imayadismas@gmail.com>
2016-02-03 13:21:26 +0300

    loopcontrols.py edited

--- loopcontrols.py
+++ loopcontrols.py
@@ -5,21 +5,24 @@
 # for index, line in enumerate(fh.readlines()):
 # 	print(index, line)
 def main():
-
+	print("Display the character")
 	s = 'this is a string'
 	for c in s:
 		print(c)#each character at a time
 
+	print("Display the character and shortcuts the loop")
 	s = 'this is a string'
 	for c in s:
 		if c == 's': continue #shortcuts the loop. Goes back to the loop and gets another iteration
 		print(c)#each character at a time
 
+	print("Display the character and using break to jump out of the loop")
 	s = 'this is a string'
 	for c in s:
 		if c == 's': break#jumps out of the loop entirely
 		print(c)#each character at a time
 
+	print("Display the character using else if the condition fails")
 	s = 'this is a string'
 	for c in s:
 		print(c)#each character at a time
@@ -27,6 +30,7 @@ def main():
 		print('else')
 
 
+	print("Display the character from an array of strings using while")
 	s = 'this is a string'
 	i = 0
 	while(i < len(s)):
