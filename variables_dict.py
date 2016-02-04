a28dcd7c09e4ae476ed1cf75c24f5618204a8a8f
ImayaDismas <imayadismas@gmail.com>
2016-02-04 08:58:57 +0300

    Use of dictionaries as mutable objects

--- variables_dict.py
+++ variables_dict.py
@@ -2,7 +2,14 @@
 
 def main():
 	d = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
-	for k in sorted(d.keys()):
+	for k in sorted(d.keys()): #sorted sorts the alphabetically
 		print(k, d[k])
+	#using a different definition of the dictionaries
+	a = dict(
+			one=1, two = 2, three = 3, four = 4, five = 'five'
+		)#usually are mutable
+	a['seven'] = 7#one can add this
+	for k in sorted(a.keys()): #sorted sorts the alphabetically
+		print(k, a[k])
 
 if __name__ == "__main__": main()
