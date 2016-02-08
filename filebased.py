5202beb43fb91ee79996b53c121f12f3d204d483
ImayaDismas <imayadismas@gmail.com>
2016-02-09 01:38:11 +0300

    the logic complete

--- filebased.py
+++ filebased.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
+# Pattern: (d\w+)\W(d\w+)
+
+import re
+def searching():
+  file = open("filebased.txt", "r")
+  words = input("Enter the words to search:\n")
+  for line in file:
+    # match = re.search(words, line)
+    m = re.match(words + "\w+", line)
+    # m = re.match(words + "\w+\W\w+", line)
+    if m:
+      print(m.group())
+    
+
+  file.close()
+
 def storage():
-    file = open("filebased.txt", "w")
+    file = open("filebased.txt", "a")
     words = input("Enter the words to store:\n")
     file.write(words)
+    file.write("\n")
     file.close()
 
 
@@ -11,13 +28,13 @@ def  main():
   option = ''
   while option != 'end':
     option = input("Enter your choice:\n 'store' to Store in the database \n 'find' to Search in the database\n 'end' to quit\n")
-    if option == "store":
+    if option == "store" or option == "Store":
       storage()
 
-    elif option == "find":
+    elif option == "find" or option == "Find":
       searching()
 
-    elif option == 'end':
+    elif option == 'end' or option == 'End':
         print("\nGoodbye. See you later.\n")
 
     else:
