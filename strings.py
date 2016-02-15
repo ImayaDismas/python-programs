3018cde6dad762d1d2f0d970986567820ea18f62
ImayaDismas <imayadismas@gmail.com>
2016-02-16 01:17:34 +0300

    string objects

---
+++ strings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+	str = 'this is a string'
+	print(str)
+	print(str.upper())
+	print('this is a string {}'.format(42))
+	print('this is a string %d' %432)
+
+
+if __name__ == "__main__":main()
